79b13abda2572634622d4e697d92d6b0410a5c3b
ricardopraxedes <ricardopraxedes1@gmail.com>
2021-04-15 12:00:08 -0300

    fix: include next on error middleware

--- src/shared/infra/http/index.ts
+++ src/shared/infra/http/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import "reflect-metadata";
 import swaggerUi from "swagger-ui-express";
@@ -18,14 +18,16 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
-app.use((error: Error, request: Request, response: Response) => {
-  if (error instanceof AppError) {
-    return response.status(error.statusCode).json({ message: error.message });
-  }
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({ message: error.message });
+    }
 
-  return response
-    .status(500)
-    .json({ message: `Internal Server Error - ${error.message}` });
-});
+    return response
+      .status(500)
+      .json({ message: `Internal Server Error - ${error.message}` });
+  }
+);
 
 export { app };
